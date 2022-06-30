67db52cac7e3e7d4f40930aad511fd93ac80d139
“anjanavishvanath” <anjanavishvanath3@gmail.com>
2022-06-30 06:38:58 +0530

    Codewars 7kyu need further amendments

--- cw_28.06.2022/main.js
+++ cw_28.06.2022/main.js
@@ -14,9 +14,9 @@ function sorted(arr){
     let newArr = arr.map(x=>x).sort((a,b)=>a-b)//accending
     let newArrTwo = arr.map(x=>x).sort((a,b)=>b-a)//decending
 
-    console.log(`arr : ${arr}`)
-    console.log(`newArr : ${newArr}`)
-    console.log(`newArrTwo : ${newArrTwo}`)
+    // console.log(`arr : ${arr}`)
+    // console.log(`newArr : ${newArr}`)
+    // console.log(`newArrTwo : ${newArrTwo}`)
 
     if(arr == newArr){
         return 'yes,accending'
