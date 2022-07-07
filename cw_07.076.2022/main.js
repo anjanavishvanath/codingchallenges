580c53cb21d4386057b664f4cdf175f7cc8c5baf
“anjanavishvanath” <anjanavishvanath3@gmail.com>
2022-07-07 05:56:39 +0530

    amendments...

--- cw_07.076.2022/main.js
+++ cw_07.076.2022/main.js
@@ -1,14 +1,35 @@
 
 
-function calc(str){
-    let word = str.split(" ")
+function dotCalculator(equation){
+    let word = equation.split(" ")
+    let newlen = 0
+    let result = ""
     if(word[1] == "+"){
-        return word[0].length + word[2].length
+        newlen = word[0].length + word[2].length
+        for(let i=0;i<newlen;i++){
+            result += "."
+        }
+        return result
     }else if(word[1] == "-"){
-        return word[0].length - word[2].length
+        newlen = word[0].length - word[2].length
+        for(let i=0;i<newlen;i++){
+            result += "."
+        }
+        return result
     }else if(word[1] == "*"){
-        return word[0].length * word[2].length
+        newlen = word[0].length * word[2].length
+        for(let i=0;i<newlen;i++){
+            result += "."
+        }
+        return result
+
     }else if(word[1] == "//"){
-        return word[0].length / word[2].length
+        newlen = Math.round(word[0].length + word[2].length)
+        console.log(newlen)
+        for(let i=0;i<newlen;i++){
+            result += "."
+        }
+        return result
+
     }
 }
