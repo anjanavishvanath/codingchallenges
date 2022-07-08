5488aaff3d76f5292e61d5998abe4a82a7f602f3
“anjanavishvanath” <anjanavishvanath3@gmail.com>
2022-07-08 10:48:53 +0530

    finished the chalange on 7th

--- cw_07.076.2022/main.js
+++ cw_07.076.2022/main.js
@@ -1,35 +1,30 @@
 
-
 function dotCalculator(equation){
-    let word = equation.split(" ")
-    let newlen = 0
+    let word =  equation.split(" ")
     let result = ""
+
     if(word[1] == "+"){
-        newlen = word[0].length + word[2].length
-        for(let i=0;i<newlen;i++){
+        for(let i=0;i<word[0].length+word[2].length;i++){
             result += "."
         }
-        return result
+        return (result)
     }else if(word[1] == "-"){
-        newlen = word[0].length - word[2].length
-        for(let i=0;i<newlen;i++){
+        for(let i=0;i<word[0].length-word[2].length;i++){
             result += "."
         }
-        return result
+        return (result)
     }else if(word[1] == "*"){
-        newlen = word[0].length * word[2].length
-        for(let i=0;i<newlen;i++){
+        for(let i=0;i<word[0].length*word[2].length;i++){
             result += "."
         }
-        return result
-
+        return (result)
     }else if(word[1] == "//"){
-        newlen = Math.round(word[0].length + word[2].length)
-        console.log(newlen)
-        for(let i=0;i<newlen;i++){
+        for(let i=0;i<Math.floor(word[0].length/word[2].length);i++){
             result += "."
         }
-        return result
-
+        return (result)
     }
 }
+
+
+console.log(dotCalculator("..... + .."))
